bf071a8d78d6ddf7d457abcbc71f4d15488ee8b3
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-03-30 16:04:23 +0300

    Update files

--- tests/Tele2_func_positive.py
+++ tests/Tele2_func_positive.py
@@ -8,16 +8,15 @@ from conftest import driver_auth, driver_no_auth
 import time
 
 
-class TestTele2_Functional_Auth_Off_Positive:
+class TestTele2_Functional_Auth_ON_Positive:
     """Класс с коллекцией positive UI-тестов функционального тестирования веб-сайта оператора "Tele2" в режиме
     авторизованного пользователя."""
 
-    def test_private_person_button_click(self, driver_auth):
+    def test_private_persons_button_click(self, driver_auth):
         """"""
         page = HomePage(driver_auth)
         url_before = page.get_relative_link()
         page.private_clients_btn_click()
         url_after = page.get_relative_link()
-        page.wait_page_loaded()
         print(url_before, url_after, sep=', ')
         assert url_before != url_after
