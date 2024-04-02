a8696961e6641a4a07ac972b0d874f63ba35b007
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-04-02 15:54:58 +0300

    Update files

--- tests/home_no_auth_positive.py
+++ tests/home_no_auth_positive.py
@@ -25,14 +25,19 @@ class TestTele2_Functional_Auth_OFF_Positive:
     def test_private_persons_button_click(self, driver):
         """Тест работы элемента 'Частным лицам' в главном меню навигации сайта. Ожидаемый результат - переход на
         страницу с path='/home'."""
-
-        page = HomePage(driver)
-        page.private_clients_btn_click(driver)
-        page.wait_page_loaded()
-        page.checking_for_a_popup_menu(driver)
-        current_url = page.get_relative_link()
-
-        assert current_url == '/home'
+        with allure.step("Шаг 1: Открыть страницу URL=https://msk.tele2.ru/home и дождаться полной загрузки всех "
+                         "элементов."):
+            page = HomePage(driver)
+            page.wait_page_loaded()
+        with allure.step("Шаг 2: Нажать элемент 'Частным лицам' в главном меню навигации сайта."):
+            page.private_clients_btn_click(driver)
+            page.checking_for_a_popup_menu(driver)
+            current_url = page.get_relative_link()
+        with allure.step("Шаг 3: Выполнить проверку ожидаемого результата"):
+            assert current_url == '/home'
+            page.make_screenshot(file_path=screenshots_folder + "\\private_persons_button_click_EXPECTED_RES.png")
+            allure.attach(page.get_page_screenshot_PNG(), name="private_persons_button_click_EXPECTED_RES",
+                          attachment_type=allure.attachment_type.PNG)
 
     @pytest.mark.more_details
     @allure.severity(allure.severity_level.CRITICAL)
@@ -49,11 +54,8 @@ class TestTele2_Functional_Auth_OFF_Positive:
         with allure.step("Шаг 1: Открыть страницу URL=https://msk.tele2.ru/home и дождаться полной загрузки всех "
                          "элементов."):
             page = HomePage(driver)
-            page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_HOME.png")
             main_tab = page.get_current_tab_ID_descriptor()
             current_path = page.get_relative_link()
-            allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_/HOME",
-                          attachment_type=allure.attachment_type.PNG)
         with allure.step("Шаг 2: Нажать на элемент 'Подробнее'"):
             page.more_details_btn_click(driver)
             page.switch_to_new_browser_tab(starting_page=main_tab)
@@ -67,10 +69,10 @@ class TestTele2_Functional_Auth_OFF_Positive:
         with allure.step("Шаг 5: Нажать на белую точку(benefits)"):
             page.benefits_dot_btn_click(driver)
             second_path = page.get_relative_link()
-        with allure.step("Шаг 5: Нажать на элемент 'Подключиться'"):
+        with allure.step("Шаг 6: Нажать на элемент 'Подключиться'"):
             page.connect_to_btn_click(driver)
             tariffs_path = page.get_relative_link()
-        with allure.step("Шаг 6: Выполнить проверку ожидаемого результата"):
+        with allure.step("Шаг 7: Выполнить проверку ожидаемого результата"):
             if current_path != second_path:
                 assert tariffs_path == '/tariffs'
                 page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_EXPECTED_RES.png")
