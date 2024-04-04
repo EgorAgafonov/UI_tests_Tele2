51e918e18d9aea0fe2b57152c47406e4caedd2f2
Yegoriktoporik <agafonov.egor.1985@gmail.com>
2024-04-04 13:26:51 +0300

    Update files

--- tests/home_no_auth_positive.py
+++ tests/home_no_auth_positive.py
@@ -38,56 +38,40 @@ class TestTele2_Functional_Auth_OFF_Positive:
             allure.attach(page.get_page_screenshot_PNG(), name="private_persons_button_click_EXPECTED_RES",
                           attachment_type=allure.attachment_type.PNG)
 
-    @pytest.mark.skip('Тестировать только в момент наличия соответствующего проморолика на сайте!!!')
     @pytest.mark.more_details
     @allure.severity(allure.severity_level.CRITICAL)
-    @allure.story("Проверка ссылок на ресурс https://evolution.tele2.ru/#about")
+    @allure.story("Проверка переадресации на кнопке 'Подробнее'")
     @allure.title("Работа кнопки 'Подробнее' на странице path=/home")
     @allure.testcase("https://msk.tele2.ru/", "TC-TELE2-MORE_DTLS")
     @allure.link("https://msk.tele2.ru/home", name="https://msk.tele2.ru/home")
     def test_more_details_button_links(self, driver):
         """Тест работы элемента 'Подробнее' на странице path='/home'. Валидация теста успешна, если при нажатии
-        элемента 'Подробнее' браузер открывает новую вкладку по URL = https://evolution.tele2.ru/#about c рекламным
-        блоком услуг Tele2. При воздействии на элемент 'Подключиться' система адресует пользователя на URl =
-        https://msk.tele2.ru/tariffs c действующими тарифами оператора связи."""
+        элемента 'Подробнее' система адресует пользователя на ресурс с рекламной информацией о текущем тарифе Tele2."""
 
         with allure.step("Шаг 1: Открыть страницу URL=https://msk.tele2.ru/home и дождаться полной загрузки всех "
                          "элементов."):
             page = HomePage(driver)
-            main_tab = page.get_current_tab_ID_descriptor()
             current_path = page.get_relative_link()
+            page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_BEFORE.png")
+            allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_BEFORE",
+                          attachment_type=allure.attachment_type.PNG)
         with allure.step("Шаг 2: Нажать на элемент 'Подробнее'"):
             page.more_details_btn_click(driver)
-            page.switch_to_new_browser_tab(starting_page=main_tab)
-            page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_ABOUT.png")
-            allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_/#ABOUT",
-                          attachment_type=allure.attachment_type.PNG)
-        with allure.step("Шаг 3: На открывшейся вкладке окна нажать на белую точку(about) в левой части экрана"):
-            page.about_dot_btn_click(driver)
-        with allure.step("Шаг 4: Нажать на белую точку(mia)"):
-            page.mia_dot_btn_click(driver)
-        with allure.step("Шаг 5: Нажать на белую точку(benefits)"):
-            page.benefits_dot_btn_click(driver)
             second_path = page.get_relative_link()
-        with allure.step("Шаг 6: Нажать на элемент 'Подключиться'"):
-            page.connect_to_btn_click(driver)
-            tariffs_path = page.get_relative_link()
-        with allure.step("Шаг 7: Выполнить проверку ожидаемого результата"):
+        with allure.step("Шаг 3: Выполнить проверку ожидаемого результата"):
             if current_path != second_path:
-                assert tariffs_path == '/tariffs'
-                page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_PASSED.png")
-                allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_PASSED",
+                page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_AFTER.png")
+                allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_AFTER",
                               attachment_type=allure.attachment_type.PNG)
-                page.close_current_browser_tab()
-                page.switch_back_to_main_tab(main_window_id=main_tab)
+                page.private_clients_btn_click(driver)
+                page.wait_page_loaded()
             else:
-                print('Ошибка! Проверить работу ссылки элемента "Подключиться" на странице '
-                      'URL=https://evolution.tele2.ru/#about.')
+                print('Ошибка! Проверить работу ссылки кнопки "Подробнее" на странице path=/home')
                 page.make_screenshot(file_path=screenshots_folder + "\\more_details_button_links_FAILED.png")
                 allure.attach(page.get_page_screenshot_PNG(), name="more_details_button_links_FAILED",
                               attachment_type=allure.attachment_type.PNG)
-                page.close_current_browser_tab()
-                page.switch_back_to_main_tab(main_window_id=main_tab)
+                page.private_clients_btn_click(driver)
+                page.wait_page_loaded()
 
     @pytest.mark.tariff_price
     @allure.severity(allure.severity_level.CRITICAL)
@@ -135,17 +119,17 @@ class TestTele2_Functional_Auth_OFF_Positive:
 @allure.label(LabelType.FRAMEWORK, "Pytest", "Selenium")
 class TestTele2_Functional_Auth_ON_Positive:
     """Класс с коллекцией positive UI-тестов функционального тестирования веб-сайта оператора "Tele2" в режиме
-    неавторизованного пользователя."""
+    авторизованного пользователя."""
 
     @pytest.mark.private_persons
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.story("Проверка навигационного меню сайта")
     @allure.title("Работа кнопки 'Частным лицам' на странице path=/")
-    @allure.testcase("https://msk.tele2.ru/", "TC-TELE2-NAVMENU-01")
+    @allure.testcase("https://msk.tele2.ru/", "TC-TELE2-NAVMENU-01_AUTH")
     @allure.link("https://msk.tele2.ru/", name="https://msk.tele2.ru/")
     def test_private_persons_button_click(self, driver_auth):
-        """Тест работы элемента 'Частным лицам' в главном меню навигации сайта. Ожидаемый результат - переход на
-        страницу с path='/home'."""
+        """Тест работы элемента 'Частным лицам' в главном меню навигации сайта в режиме авторизованного пользователя.
+        Ожидаемый результат - переход на страницу с path='/home'."""
 
         with allure.step("Шаг 1: Открыть страницу https://msk.tele2.ru/home"):
             page = HomePage(driver_auth)
@@ -161,15 +145,14 @@ class TestTele2_Functional_Auth_ON_Positive:
 
     @pytest.mark.more_details
     @allure.severity(allure.severity_level.CRITICAL)
-    @allure.story("Проверка ссылок на ресурс https://evolution.tele2.ru/#about")
+    @allure.story("Проверка переадресации на кнопке 'Подробнее'")
     @allure.title("Работа кнопки 'Подробнее' на странице path=/home")
-    @allure.testcase("https://msk.tele2.ru/", "TC-TELE2-MORE_DTLS")
+    @allure.testcase("https://msk.tele2.ru/", "TC-TELE2-MORE_DTLS_AUTH")
     @allure.link("https://msk.tele2.ru/home", name="https://msk.tele2.ru/home")
     def test_more_details_button_links(self, driver_auth):
-        """Тест работы элемента 'Подробнее' на странице path='/home'. Валидация теста успешна, если при нажатии
-        элемента 'Подробнее' браузер открывает новую вкладку по URL = https://evolution.tele2.ru/#about c рекламным
-        блоком услуг Tele2. При воздействии на элемент 'Подключиться' система адресует пользователя на URl =
-        https://msk.tele2.ru/tariffs c действующими тарифами оператора связи."""
+        """Тест работы элемента 'Подробнее' на странице path='/home' в режиме авторизованного пользователя. Валидация
+        теста успешна, если при нажатии элемента 'Подробнее' система адресует пользователя на ресурс с рекламной
+        информацией о текущем тарифе Tele2."""
 
         with allure.step("Шаг 1: Открыть страницу URL=https://msk.tele2.ru/home и дождаться полной загрузки всех "
                          "элементов."):
@@ -200,13 +183,13 @@ class TestTele2_Functional_Auth_ON_Positive:
     @allure.severity(allure.severity_level.CRITICAL)
     @allure.story("Проверка калькулятора тарифов для частных лиц")
     @allure.title("Расчет стоимости тарифа при покупке более одной sim-карты")
-    @allure.testcase("https://msk.tele2.ru/home", "TC-TELE2-TARRIFS_CALC")
+    @allure.testcase("https://msk.tele2.ru/home", "TC-TELE2-TARRIFS_CALC_AUTH")
     @allure.link("https://msk.tele2.ru/home", name="https://msk.tele2.ru/home")
     def test_SIMs_quantity_price(self, driver_auth):
-        """Тест работы селектора выбора количества sim-карт на странице path='/home'. Валидация теста успешна,
-        если при последовательном нажатии на каждый элемент селектора с указанным количеством sim-карт к покупке,
-        текущая стоимость обслуживания на каждом из доступных тарифов будет уменьшаться с одновременным отображением
-        значения в карточке."""
+        """Тест работы селектора выбора количества sim-карт на странице path='/home' в режиме авторизованного
+        пользователя. Валидация теста успешна, если при последовательном нажатии на каждый элемент селектора с
+        указанным количеством sim-карт к покупке, текущая стоимость обслуживания на каждом из доступных тарифов будет
+        уменьшаться с одновременным отображением значения в карточке."""
 
         with allure.step("Шаг 1: Открыть страницу URL=https://msk.tele2.ru/home и дождаться полной загрузки всех "
                          "элементов."):
